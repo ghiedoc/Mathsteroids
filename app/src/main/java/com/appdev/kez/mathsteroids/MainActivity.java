2d370f12f1c145c88e86e5802179a7589b4b95cc
GILLIAN <g28doctor@yahoo.com>
2020-01-08 22:22:01 +0800

    added background music where tou can turn on and off.

--- app/src/main/java/com/appdev/kez/mathsteroids/MainActivity.java
+++ app/src/main/java/com/appdev/kez/mathsteroids/MainActivity.java
@@ -1,9 +1,15 @@
 package com.appdev.kez.mathsteroids;
 
 import android.animation.ValueAnimator;
+import android.content.ComponentName;
+import android.content.Context;
 import android.content.DialogInterface;
 import android.content.Intent;
+import android.content.ServiceConnection;
+import android.content.SharedPreferences;
 import android.media.MediaPlayer;
+import android.os.IBinder;
+import android.os.PowerManager;
 import android.support.annotation.MainThread;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
@@ -20,13 +26,13 @@ import com.thekhaeng.pushdownanim.PushDownAnim;
 
 public class MainActivity extends AppCompatActivity {
 
-    Button playBtn, exitBtn;
-    ImageView ivAbout, ivSound;
-    MediaPlayer mediaPlayer;
-
-    //Initialize Class
     private SoundPlayer sound;
 
+    Button playBtn, exitBtn;
+    ImageView ivAbout, ivSound;
+    HomeWatcher mHomeWatcher;
+    int musicCounter = 0;
+    
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -48,14 +54,14 @@ public class MainActivity extends AppCompatActivity {
 
         PushDownAnim.setPushDownAnimTo(playBtn)
                 .setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                sound.playClicked();
-                Toast.makeText(MainActivity.this, "Play", Toast.LENGTH_SHORT).show();
-                Intent intent = new Intent(MainActivity.this, com.appdev.kez.mathsteroids.GameActivity.class);
-                startActivity(intent);
-            }
-        });
+                    @Override
+                    public void onClick(View v) {
+                        sound.playClicked();
+                        Toast.makeText(MainActivity.this, "Play", Toast.LENGTH_SHORT).show();
+                        Intent intent = new Intent(MainActivity.this, com.appdev.kez.mathsteroids.GameActivity.class);
+                        startActivity(intent);
+                    }
+                });
 
         PushDownAnim.setPushDownAnimTo(exitBtn).setOnClickListener(new View.OnClickListener() {
             @Override
@@ -102,6 +108,146 @@ public class MainActivity extends AppCompatActivity {
             }
         });
         animator.start();
+
+        //Background Music
+        SharedPreferences loadToggleState = this.getSharedPreferences("MyMusic", Context.MODE_PRIVATE);
+        musicCounter = loadToggleState.getInt("music", 0); //0 is the default value
+
+        //Make sure in onCrate not to have duplicate `doBindService();` you need to call it only once like below!
+        if (musicCounter == 0) {
+            doBindService();
+            Intent music = new Intent();
+            music.setClass(this, BackgroundMusicService.class);
+            startService(music);
+        }
+
+        /**
+         * Background Music Service
+         * BIND Music Service first
+         */
+
+        /**
+         * Start HomeWatcher
+         */
+        //Start HomeWatcher
+        mHomeWatcher = new HomeWatcher(this);
+        mHomeWatcher.setOnHomePressedListener(new HomeWatcher.OnHomePressedListener() {
+            @Override
+            public void onHomePressed() {
+                if (mServ != null) {
+                    mServ.pauseMusic();
+                }
+            }
+
+            @Override
+            public void onHomeLongPressed() {
+                if (mServ != null) {
+                    mServ.pauseMusic();
+                }
+            }
+        });
+        mHomeWatcher.startWatch();
+
+        PushDownAnim.setPushDownAnimTo(ivSound).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (musicCounter > 0) {
+                    doBindService();
+                    Intent music = new Intent();
+                    music.setClass(MainActivity.this, BackgroundMusicService.class);
+                    startService(music);
+                    musicCounter = 0;
+                    if (mServ != null) {
+                        mServ.startMusic();
+                        ivSound.setImageResource(R.drawable.sound_on_white);
+                    }
+                } else {
+                    musicCounter = 1;
+                    if (mServ != null) {
+                        mServ.stopMusic();
+                        ivSound.setImageResource(R.drawable.sound_off);
+                    }
+                }
+                SharedPreferences saveMusic = getSharedPreferences("MyMusic", Context.MODE_PRIVATE);
+                SharedPreferences.Editor editorMusic = saveMusic.edit();
+                editorMusic.putInt("music", musicCounter);
+                editorMusic.apply();
+            }
+        });
+
+    }
+
+    /**
+     * Bind or Unbind the Background Music
+     */
+    private boolean mIsBound = false;
+    private BackgroundMusicService mServ;
+
+    /**
+     * Background Music
+     */
+    private ServiceConnection Scon = new ServiceConnection() {
+
+        public void onServiceConnected(ComponentName name, IBinder
+                binder) {
+            mServ = ((BackgroundMusicService.ServiceBinder) binder).getService();
+        }
+
+        public void onServiceDisconnected(ComponentName name) {
+            mServ = null;
+        }
+    };
+
+    void doBindService() {
+        bindService(new Intent(this, BackgroundMusicService.class),
+                Scon, Context.BIND_AUTO_CREATE);
+        mIsBound = true;
+    }
+
+    void doUnbindService() {
+        if (mIsBound) {
+            unbindService(Scon);
+            mIsBound = false;
+        }
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+
+        if (mServ != null) {
+            mServ.resumeMusic();
+        }
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+
+        //Detect idle screen
+        PowerManager pm = (PowerManager)
+                getSystemService(Context.POWER_SERVICE);
+        boolean isScreenOn = false;
+        if (pm != null) {
+            isScreenOn = pm.isScreenOn();
+        }
+
+        if (!isScreenOn) {
+            if (mServ != null) {
+                mServ.pauseMusic();
+            }
+        }
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+
+        //UNBIND music service
+        doUnbindService();
+        Intent music = new Intent();
+        music.setClass(this, BackgroundMusicService.class);
+        stopService(music);
 
     }
 }
